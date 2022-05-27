416ccb2ea933f027755720a26fe0c195229a6f7e
Caio <caiobulha@gmail.com>
2022-05-27 14:48:01 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,81 @@
+var corpo = document.getElementById("corpo")
+
+function red() {
+    if(corpo.classList.contains('red')) {
+        corpo.classList.remove('red');
+    } else {
+        corpo.style.transitionDuration = ".5s";
+        corpo.className = '';
+        corpo.classList.add('red');
+    }
+}
+
+function green() {
+    if(corpo.classList.contains('green')) {
+        corpo.classList.remove('green')
+    } else {
+        corpo.style.transitionDuration = ".5s"
+        corpo.className = '';
+        corpo.classList.add('green')
+    }
+}
+
+function yellow() {
+    if(corpo.classList.contains('yellow')) {
+        corpo.classList.remove('yellow')
+    } else {
+        corpo.style.transitionDuration = ".5s"
+        corpo.className = '';
+        corpo.classList.add('yellow')
+    }
+}
+
+function blue() {
+    if(corpo.classList.contains('blue')) {
+        corpo.classList.remove('blue')
+    } else {
+        corpo.style.transitionDuration = ".5s"
+        corpo.className = '';
+        corpo.classList.add('blue')
+    }
+}
+
+function pink() {
+    if(corpo.classList.contains('pink')) {
+        corpo.classList.remove('pink')
+    } else {
+        corpo.style.transitionDuration = ".5s"
+        corpo.className = '';
+        corpo.classList.add('pink')
+    }
+}
+
+function purple() {
+    if(corpo.classList.contains('pruple')) {
+        corpo.classList.remove('purple')
+    } else {
+        corpo.style.transitionDuration = ".5s"
+        corpo.className = '';
+        corpo.classList.add('purple')
+    }
+}
+
+function yellow() {
+    if(corpo.classList.contains('yellow')) {
+        corpo.classList.remove('yellow')
+    } else {
+        corpo.style.transitionDuration = ".5s"
+        corpo.className = '';
+        corpo.classList.add('yellow')
+    }
+}
+
+function rgb() {
+    if(corpo.classList.contains('rgb')) {
+        corpo.classList.remove('rgb')
+    } else {
+        corpo.style.transitionDuration = ".5s"
+        corpo.className = '';
+        corpo.classList.add('rgb')
+    }
+}
